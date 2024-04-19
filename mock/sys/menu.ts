c6ec024bdb3b402480dc00aa90a9f26a0692dfc5
cszhjh <cszhjh@gmail.com>
2024-04-19 13:17:28 +0800

    feat(setting): 空白页bug复现

--- mock/sys/menu.ts
+++ mock/sys/menu.ts
@@ -6,39 +6,40 @@ import { createFakeUserList } from './user';
 const dashboardRoute = {
   path: '/dashboard',
   name: 'Dashboard',
-  component: 'LAYOUT',
-  redirect: '/dashboard/analysis',
+  // component: 'LAYOUT',
+  component: '/dashboard/analysis/index',
+  // redirect: '/dashboard/analysis',
   meta: {
     title: 'routes.dashboard.dashboard',
-    hideChildrenInMenu: true,
+    // hideChildrenInMenu: true,
     icon: 'bx:bx-home',
   },
-  children: [
-    {
-      path: 'analysis',
-      name: 'Analysis',
-      component: '/dashboard/analysis/index',
-      meta: {
-        hideMenu: true,
-        hideBreadcrumb: true,
-        title: 'routes.dashboard.analysis',
-        currentActiveMenu: '/dashboard',
-        icon: 'bx:bx-home',
-      },
-    },
-    {
-      path: 'workbench',
-      name: 'Workbench',
-      component: '/dashboard/workbench/index',
-      meta: {
-        hideMenu: true,
-        hideBreadcrumb: true,
-        title: 'routes.dashboard.workbench',
-        currentActiveMenu: '/dashboard',
-        icon: 'bx:bx-home',
-      },
-    },
-  ],
+  // children: [
+  //   {
+  //     path: 'analysis',
+  //     name: 'Analysis',
+  //     component: '/dashboard/analysis/index',
+  //     meta: {
+  //       hideMenu: true,
+  //       hideBreadcrumb: true,
+  //       title: 'routes.dashboard.analysis',
+  //       currentActiveMenu: '/dashboard',
+  //       icon: 'bx:bx-home',
+  //     },
+  //   },
+  //   {
+  //     path: 'workbench',
+  //     name: 'Workbench',
+  //     component: '/dashboard/workbench/index',
+  //     meta: {
+  //       hideMenu: true,
+  //       hideBreadcrumb: true,
+  //       title: 'routes.dashboard.workbench',
+  //       currentActiveMenu: '/dashboard',
+  //       icon: 'bx:bx-home',
+  //     },
+  //   },
+  // ],
 };
 
 const backRoute = {
@@ -253,11 +254,11 @@ export default [
       let menu: Object[];
       switch (id) {
         case '1':
-          dashboardRoute.redirect = dashboardRoute.path + '/' + dashboardRoute.children[0].path;
+          // dashboardRoute.redirect = dashboardRoute.path + '/' + dashboardRoute.children[0].path;
           menu = [dashboardRoute, authRoute, levelRoute, sysRoute, linkRoute];
           break;
         case '2':
-          dashboardRoute.redirect = dashboardRoute.path + '/' + dashboardRoute.children[1].path;
+          // dashboardRoute.redirect = dashboardRoute.path + '/' + dashboardRoute.children[1].path;
           menu = [dashboardRoute, authRoute, levelRoute, linkRoute];
           break;
         default:
